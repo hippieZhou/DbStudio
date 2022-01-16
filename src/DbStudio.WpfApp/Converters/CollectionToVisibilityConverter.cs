9166daf5d9b0d80d1e08d994d15c4c078a9c6508
hippieZhou <hippiezhou@outlook.com>
2022-01-16 10:52:59 +0800

    update

---
+++ src/DbStudio.WpfApp/Converters/CollectionToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using DbStudio.WpfApp.Models;
+
+namespace DbStudio.WpfApp.Converters
+{
+    public class CollectionToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is ObservableCollection<DbConnection> collection
+                ? collection.Any() ? Visibility.Visible : Visibility.Collapsed
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
